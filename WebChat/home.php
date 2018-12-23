0099289adfb3af7a1df40b45beb0f8205d8fbbe5
Supercolbat <supercolbat@gmail.com>
2018-12-23 12:25:07 -0600

    Beautified code
    ~Your welcome Dolphinbob

--- WebChat/home.php
+++ WebChat/home.php
@@ -1,33 +1,25 @@
 <!DOCTYPE html>
-
 <html>
-<head>
-	
-
-</head>
-<body>
-<div id="main">
-
-<h1 style="background-color: #6495ed;color: white;">-online</h1>
-
-</div class="output">
-
-</div>
-
-<form method="post" action="send.php">
-<textarea name="msg" placeholder="blah blah blah..." class="form-control"></textarea><br>
-<input type="submit" valuse="Send">
-
-</form>
-<br>
-
-<form action="logout.php">
-
-<input style="width: 100%;background-color: #6495ed;color: white;font-size: 20px;" type="submit," name="Logout">
-
-</form>
-
-</div> 
-
-</body>
+    <head>
+    	<title></title>
+    	<style>
+    		
+    	</style>
+    </head>
+    <body>
+        <div id="main">
+            <h1 style="background-color: #6495ed;color: white;">Group Name</h1> <!-- Group name -->
+            <div id="output">
+        	    <!--Code...-->
+            </div>
+            <form method="post" action="send.php">
+                <textarea name="msg" placeholder="Type in your message..." class="form-control"></textarea><br />
+                <input type="submit" value="Send" />
+            </form>
+            <br />
+            <form action="logout.php">
+                <input style="width: 100%;background-color: #6495ed;color: white;font-size: 20px;" type="submit," name="Logout">
+            </form>
+        </div> 
+    </body>
 </html>
