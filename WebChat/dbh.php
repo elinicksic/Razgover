cccef0fbf45e4ff816dde37f35e08ff172f6a6e7
Supercolbat <supercolbat@gmail.com>
2018-12-23 11:24:26 -0600

    Update dbh.php
    Created a public server to hold the user data instead of having a local database.

--- WebChat/dbh.php
+++ WebChat/dbh.php
@@ -1,10 +1,9 @@
 <?php
 
-$conn=mysqli_connect("localhost", "root", "", "webchat");
+$conn=mysqli_connect("73.32.192.74", "root", "", "webchat");
+//$conn=mysqli_connect("localhost", "root", "", "webchat");
 if(!$conn){
-
 	die("connection failed".mysqli_connect_error());
 }
-
 
 ?>
