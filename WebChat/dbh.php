57c367006f7f740b8ff5b72ba41c96955386ac2c
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2018-12-23 12:26:45 -0600

    Removed unnececary comment

--- WebChat/dbh.php
+++ WebChat/dbh.php
@@ -1,7 +1,6 @@
 <?php
 
 $conn=mysqli_connect("73.32.192.74", "root", "", "webchat");
-//$conn=mysqli_connect("localhost", "root", "", "webchat");
 if(!$conn){
 	die("connection failed".mysqli_connect_error());
 }
