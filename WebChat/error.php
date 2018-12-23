bed87d1fdb52dc599d1ba65d2073a0a4ca8adbe2
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2018-12-22 21:30:23 -0600

    Made login more secure and add error.php
    add mysqli_real_escape_string to username and password input

---
+++ WebChat/error.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Error</title>
+</head>
+<body>
+<h1>Username or Password was not found</h1>
+<p>Please try again <a href="index.php">here</a></p>
+</body>
+</html>
--- WebChat/login.php
+++ WebChat/login.php
@@ -2,11 +2,12 @@
 session_start();
 include "dbh.php";
 
-$uname=$_POST["uname"];
-$pass=$_POST["pass"];
-
-
 $sql="SELECT * FROM signup WHERE username='$uname' AND password='$pass'";
+
+$uname=mysqli_real_escape_string($conn, $_POST["uname"]);
+$pass=mysqli_real_escape_string($conn, $_POST["pass"]);
+
+
 $result=$conn->query($sql);
 
 if(!$row = $result->fetch_assoc()){
