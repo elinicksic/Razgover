71f13dc781c5c9144ed8db278ad8c44145a9e09e
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2018-12-24 11:29:04 -0600

    Made error.php prettey

--- Chattify/error.php
+++ Chattify/error.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Error</title>
-</head>
-<body>
-<h1>Username or Password was not found</h1>
-<p>Please try again <a href="index.php">here</a></p>
-</body>
+	<head>
+		<title>Error</title>
+	</head>
+	<body>
+		<h1>Username or Password was not found</h1>
+		<p>Please try again <a href="index.php">here</a></p>
+	</body>
 </html>
