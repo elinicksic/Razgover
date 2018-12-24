c5b11a3320c37010598172b17202941814d6b190
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2018-12-24 09:04:15 -0600

    Added password hashing
    Passwords are now hashed and salted before going to the database.

--- Chattify/login.php
+++ Chattify/login.php
@@ -2,15 +2,16 @@
 session_start();
 include "dbh.php";
 
-$sql="SELECT * FROM signup WHERE username='$uname' AND password='$pass'";
-
 $uname=mysqli_real_escape_string($conn, $_POST["uname"]);
 $pass=mysqli_real_escape_string($conn, $_POST["pass"]);
 
 
-$result=$conn->query($sql);
+$sql="SELECT * FROM signup WHERE username='$uname'";
 
-if(!$row = $result->fetch_assoc()){
+$result=mysqli_query($conn, $sql);
+$row=mysqli_fetch_assoc($result);
+
+if(!password_verify($pass, $row["password"])){
 	header("Location:error.php");
 
 } else {
--- Chattify/signup.php
+++ Chattify/signup.php
@@ -3,7 +3,7 @@
 include "dbh.php";
 $uname=$_POST["uname"];
 $email=$_POST["email"];
-$pass=$_POST["pass"];
+$pass=password_hash($_POST["pass"], PASSWORD_BCRYPT);
 
 
 $sql="insert into signup(username,email,password)
