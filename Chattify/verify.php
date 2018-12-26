c3d1f29f909712765a93419a94c1478fa868dfa6
Supercolbat <supercolbat@gmail.com>
2018-12-25 19:13:05 -0600

    CSS + account verification
    ^
     |

---
+++ Chattify/verify.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php 
+
+?>
+<html>
+    <head>
+    	<title>Chattify - Congrats!</title>
+    	<link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
+    	<fieldset>
+    	    <legend>
+    	        <center><h1>Congrats!</h1></center>
+    	    </legend>
+    	    You successfully created your account
+    	</fieldset>
+    </body>
+</html>
