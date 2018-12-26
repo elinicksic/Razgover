3fc5c81888903297f15f7ee588e0bc180423ba02
Supercolbat <supercolbat@gmail.com>
2018-12-25 19:45:35 -0600

    Help needed
    Please help me with this code

--- Chattify/home.php
+++ Chattify/home.php
@@ -8,21 +8,34 @@ $sql="SELECT * FROM signup WHERE uid='$_COOKIE[uid]'";
 $result=mysqli_query($conn, $sql);
 $row=mysqli_fetch_assoc($result);
 echo "<h1> Welcome $row[username] </h1>";
+
+$msgs="SELECT msg FROM posts";
+$result=mysqli_query($conn, $sql); //HELP
+$row=mysqli_fetch_assoc($result); //HELP
+
+/*
+IF (SAVED != CURRENT) {
+    SAVED = CURRENT;
+    display(SAVED);
+}
+*/
+while(1) {
+    if () {
+
+    }
+}
 ?>
 
 
 <html>
     <head>
     	<title>Chattify - Home</title>
-    	<style>
-    		body {
-                font-family: Arial;
-            }
-    	</style>
     </head>
     <body>
         <div id="main">
-            <h1 style="background-color: #6495ed;color: white;" id="gname"></h1> <!-- Group name -->
+            <?php
+                //echo <h1 style="background-color: #6495ed;color: white;"> $row[group]</h1>
+            ?>
             <div id="output">
         	    <?php
 
