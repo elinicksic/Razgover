09cba305f255f542ee20e9fcb0166a7c0e9a5e6c
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2019-04-14 22:12:51 -0500

    started group system

--- Chattify/load.php
+++ Chattify/load.php
@@ -1,4 +1,8 @@
 <?php
+	session_start();
+	if(!isset($_SESSION['uid'])){
+	    header("location:index.php");
+	}
 	include "dbh.php";
 	$sql = "SELECT * FROM `posts` ORDER BY date ASC";
 	$result = mysqli_query($conn, $sql);
@@ -8,6 +12,34 @@
 	    $row2 =  mysqli_fetch_assoc($result2);
 	    $sender =  htmlspecialchars($row2['username'], ENT_QUOTES, 'UTF-8');
 	    $msg =  htmlspecialchars($row['msg'], ENT_QUOTES, 'UTF-8');
-	    echo "<p><b>$sender</b><br>$msg</p>";
+	    if($row['uid'] == $_SESSION['uid']){
+	    	//sent by current user
+	    	echo("
+				<div class='media'>
+				    <div class='media-body'>
+				        <h4 class='media-heading'>$sender</h4>
+				        <p>$msg</p>
+				    </div>
+				    <div class='media-right'>
+				      <img src='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg' class='media-object' style='width:60px'>
+				    </div>
+				  </div>
+	    	");
+	    }else{
+	    	//sent by another user
+	    	echo("
+		    	<div class='media'>
+	    			<div class='media-left'>
+	      			<img src='img_avatar1.png' class='media-object' style='width:60px'>
+	    		</div>
+	    		<div class='media-body'>
+	      			<h4 class='media-heading'>$sender</h4>
+	      			<p>$msg</p>
+	    		</div>
+	  			</div>
+  				<hr>
+  			"
+  			);
+	    }
 	}   
 ?>
--- Chattify/send.php
+++ Chattify/send.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["uid"])) {
 $uid=$_SESSION["uid"];
 $msg=mysqli_real_escape_string($conn, $_POST["msg"]);
 if(trim($msg) != '') {
-    $sql="INSERT INTO posts(uid, msg) VALUES ('$uid', '$msg')";
+    $sql="INSERT INTO posts(uid, msg, gid) VALUES ('$uid', '$msg', '0')";
     $result = mysqli_query($conn, $sql);
     //echo mysqli_errno($conn) . ": " . mysqli_error($conn);
 }
