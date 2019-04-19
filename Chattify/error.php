7bfa947a514ff29104c39d2842a9f80d1a977e5e
Supercolbat <supercolbat@gmail.com>
2019-04-19 17:15:58 -0500

    No pun intended

--- Chattify/error.php
+++ Chattify/error.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Razgover - Error</title>
+        <title>Razgover - 404</title>
     </head>
-    <body>
-        <h1>Uh Oh!</h1>
-        <h2>An error has occured!</h2>
-
-        <p>Here are some possible solutions!</p>
-        <ul>
-        	<li>Check you wrote your Username and Password correctly!</li>
-        	<li>Make sure you created and verified your account</li>
-        </ul>
-        <br>
+    <body id="ePHP">
+        <fieldset>
+        <legend><h1>Whoops!</h1>h1></legend>
+            <h2>
+            <?php
+            $msgs = array("I think the internet exploded.","Surfing the web with water isn't a good idea. It'll fry the electronics.","Your shirt should be made of spider silk. Maybe that'll attract the web.","Have you been surfing the wifi lately?","You should become a ship captain because of your surfing skills.");
+            echo $msgs[array_rand($msgs)];
+            ?>
+            </h2>
+        </fieldset>
+        <h3>The page you were looking for wasn't found.</h3>
         Go back <a href="index.php">here</a>
     </body>
 </html>
