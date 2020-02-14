48b9ce4a104068f6f0b1d253cfac58814f7e730e
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2020-02-14 15:36:09 -0600

    Fixed Typo

--- Chattify/JavaScript/Login.js
+++ Chattify/JavaScript/Login.js
@@ -9,7 +9,7 @@ function tryLogin(uname, pass)
         },
         success: function(result){
             if(result == "0"){
-                $("#alerts").append("<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'>&times;</button><strong>Error</strong> Username or password dont match.</div>");
+                $("#alerts").append("<div class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'>&times;</button><strong>Error</strong> Username or password is incorrect!</div>");
             }else if(result == "1"){
                 window.location.replace("home.php");
             }
