24f2452ba24ea2dcfc5c27af2f592df1c74174f0
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2020-02-14 07:10:44 -0600

    Login using AJAX
    Instead of redirecting to a useless error page it now displays an alert on the login screen

--- Chattify/login.php
+++ Chattify/login.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="JavaScript/Login.js"></script>
 		<meta charset="utf-8"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
@@ -22,7 +23,7 @@
         <div id="main">
             <div id="info">
 		        <h2>Login</h2>
-		        <form action="include/login.inc.php" method="post">
+		        <form action="javascript:tryLogin($('#username').val(), $('#password').val());" id="loginForm">
 		            <!--<label><b>Username</b><label>
 		            <input type="text" name="uname" placeholder="Username"><br><br>
 		            <label><b>Password</b><label>
@@ -40,6 +41,9 @@
 		        </form>
                 <p>Don't have an account yet? Click <a href="signup.php">here</a> to create one!</p>
 	        </div>
+            <div id="alerts">
+
+            </div>
         </div>
     </body>
 </html>
