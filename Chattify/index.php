bcd7f66ec93666338ea69bcf9a3dc6d8b5179e34
Dolphinbob <32620307+Dolphinbob@users.noreply.github.com>
2020-02-13 21:08:51 -0600

    Changed Buttons and aded some information
    * Updated index.php to BS 4
    * Added some basic info to the index page (need work stiil)
    * Changed the login and signup buttons to be outline

--- Chattify/index.php
+++ Chattify/index.php
@@ -11,18 +11,21 @@ if(isset($_SESSION['uid'])){
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 <link rel="stylesheet" href="stylesheet.css">
 <head>
 </head>
 <body>
-<div id="topnav">
-    <h1>Welccome to Razgover</h1>
-    <a class="btn btn-primary topnavBtn" id="topnavLogin" href="login.php" role="button">Login</a>
-    <a class="btn btn-primary topnavBtn" id="topnavSignup" href="signup.php" role="button">Signup</a>
-</div>
-
-
+    <div id="topnav">
+        <h1>Welccome to Razgover</h1>
+        <a class="btn btn-outline-primary topnavBtn" id="topnavLogin" href="login.php" role="button">Login</a>
+        <a class="btn btn-outline-primary topnavBtn" id="topnavSignup" href="signup.php" role="button">Signup</a>
+    </div>
+    <div id="content">
+        <h2>What is Razgover?</h2>
+        <p>Razgover is a project built by junior high students to message each other. You can find this on github <a href="https://github.com/Razgover/Razgover">here</a>!</p>
+    </div>
 </body>
 </html>
